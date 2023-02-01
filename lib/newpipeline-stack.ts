52467a532a3c5e52531efb0f92fc6d4a0ec29658
Syed Shoaib <syed.shoaib@trangile.com>
2023-02-02 00:09:09 +0530

    service

--- lib/newpipeline-stack.ts
+++ lib/newpipeline-stack.ts
@@ -1,12 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
-import { SecretValue } from 'aws-cdk-lib';
-import { BuildSpec, LinuxBuildImage, PipelineProject } from 'aws-cdk-lib/aws-codebuild';
-import { Artifact, Pipeline } from 'aws-cdk-lib/aws-codepipeline';
-import { CloudFormationCreateUpdateStackAction, CodeBuildAction, GitHubSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
-import { Topic } from 'aws-cdk-lib/aws-sns';
+import { SecretValue, Stage } from 'aws-cdk-lib';
+import { Alarm, ComparisonOperator } from 'aws-cdk-lib/aws-cloudwatch';
+import { BuildEnvironmentVariableType, BuildSpec, LinuxBuildImage, PipelineProject } from 'aws-cdk-lib/aws-codebuild';
+import { Artifact, IStage, Pipeline } from 'aws-cdk-lib/aws-codepipeline';
+import { CloudFormationCreateUpdateStackAction, CodeBuildAction, CodeBuildActionType, GitHubSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
+import { EmailSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
 import { Construct } from 'constructs';
+import * as sns from '@aws-cdk/aws-sns';
+import { Statistic, TreatMissingData } from "aws-cdk-lib/aws-cloudwatch";
+import { SnsAction } from "aws-cdk-lib/aws-cloudwatch-actions";
+import { Topic } from "aws-cdk-lib/aws-sns";
+import { SnsTopic } from 'aws-cdk-lib/aws-events-targets';
+import { EventField, RuleTargetInput } from 'aws-cdk-lib/aws-events';
 
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class NewpipelineStack extends cdk.Stack {
 
@@ -14,20 +20,37 @@ export class NewpipelineStack extends cdk.Stack {
   private readonly cdkBuildOutput: Artifact;
   private readonly serviceBuildOutput: Artifact;
   private readonly serviceSourceOutput: Artifact;
+  private readonly cdkSourceOutput: Artifact;
   private readonly pipelineNotificationsTopic: Topic;
 
+  
+  alarmTopic: Topic;
+  
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    
     super(scope, id, props);
-
-
-    this.pipeline = new Pipeline(this, 'Pipeline', {
-      pipelineName: "Pipeline",
+    
+  
+    
+    this.pipelineNotificationsTopic = new Topic(
+      this,
+      "PipelineNotificationsTopic",
+      {
+        topicName: "PipelineNotifications",
+      }
+      );
+      this.pipelineNotificationsTopic.addSubscription(
+        new EmailSubscription("syeds7933.ss@gmail.com")
+        );
+        
+        this.pipeline = new Pipeline(this, 'Pipeline', {
+          pipelineName: "Pipeline",
       crossAccountKeys: false,
       restartExecutionOnUpdate: true,
 
     })
 
-    const cdkSourceOutput = new Artifact("CDKSourceOutput");
+    this.cdkSourceOutput = new Artifact("CDKSourceOutput");
     this.serviceSourceOutput = new Artifact("serviceSourceOutput")
 
     this.pipeline.addStage({
@@ -40,44 +63,55 @@ export class NewpipelineStack extends cdk.Stack {
         branch: "master",
         actionName: "Pipeline_Source",
         oauthToken: SecretValue.secretsManager("git_aws"),
-        output:cdkSourceOutput
+        output:this.cdkSourceOutput
         
 
        }),
       ],
     })
-
-
-
-
     this.cdkBuildOutput = new Artifact("CdkBuildOutput");
     this.serviceBuildOutput = new Artifact("ServiceBuildOutput");
 
-
-    this.pipeline.addStage({
-      stageName: "Build",
-      actions: [
-        new CodeBuildAction({
-
-          actionName: "CDK_Build",
-          input: cdkSourceOutput,
-          outputs: [this.cdkBuildOutput],
-          project: new PipelineProject(this, "CdkBuildProject", {
-            environment: {
-              buildImage: LinuxBuildImage.STANDARD_5_0,
-            },
-            buildSpec: BuildSpec.fromSourceFilename(
-              "build-specs/cdk-build-spec.yml"
-            ),
-          }),
-        }),
-
-      ],
-    });
-
-
     
 
+
+   const buildStage= this.pipeline.addStage({
+      stageName: "Build",
+      actions: [
+        // new CodeBuildAction({
+
+        //   actionName: "CDK_Build",
+        //   input: cdkSourceOutput,
+        //   outputs: [this.cdkBuildOutput],
+        //   project: new PipelineProject(this, "CdkBuildProject", {
+        //     environment: {
+          //       buildImage: LinuxBuildImage.STANDARD_5_0,
+          //     },
+        //     buildSpec: BuildSpec.fromSourceFilename(
+        //       "build-specs/cdk-build-spec.yml"
+        //       ),       
+        //     }),
+            
+    
+        //   }),    
+        
+        new CodeBuildAction({
+          actionName: "CDK_Build",
+          input: this.cdkSourceOutput,
+          outputs: [this.cdkBuildOutput],
+          project: new PipelineProject(this, "CdkBuildProject", {
+              environment: {
+              buildImage: LinuxBuildImage.STANDARD_5_0,
+              },
+              buildSpec: BuildSpec.fromSourceFilename(
+              "build-specs/cdk-build-spec.yml"
+              ),       
+          }),
+      })
+      
+      ]
+     
+    });
     this.pipeline.addStage({
       stageName: "Pipeline_Update",
       actions: [
@@ -86,15 +120,125 @@ export class NewpipelineStack extends cdk.Stack {
           stackName: "NewpipelineStack",
           templatePath: this.cdkBuildOutput.atPath("NewpipelineStack.template.json"),
           adminPermissions: true,
+          runOrder: 1,
         }),
       ],
     });
-    ///
+  }
+   public addServiceStage(
+    serviceStack: PipelineProject,
+    stageName: string
+  ): IStage {
+    return this.pipeline.addStage({
+      stageName: stageName,
+     
+    });
+    actions: [
+      new CodeBuildAction({
 
+        actionName: "CDK_Build",
+        input: this.cdkSourceOutput,
+        outputs: [this.cdkBuildOutput],
+        project: new PipelineProject(this, "CdkBuildProject", {
+          environment: {
+              buildImage: LinuxBuildImage.STANDARD_5_0,
+            },
+          buildSpec: BuildSpec.fromSourceFilename(
+            "build-specs/cdk-newman-build-spec.yml"
+            ),       
+          }),
+          
+  
+        }),  
+    ]
+  }
+
+
+
+ 
+   
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+    public addServiceTestToStage(
+      stage: IStage,
+    
+    ) {
+      const integTestAction =  new CodeBuildAction({
+        actionName: "CDK_Build",
+        input: this.cdkSourceOutput,
+        outputs: [this.cdkBuildOutput],
+        project: new PipelineProject(this, "CdkBuildProject", {
+            environment: {
+            buildImage: LinuxBuildImage.STANDARD_5_0,
+            },
+            buildSpec: BuildSpec.fromSourceFilename(
+            "build-specs/cdk-newman-build-spec.yml"
+            ),     
+
+        }),
+        type: CodeBuildActionType.TEST,
+        runOrder: 2,
+      });
+  
+      stage.addAction(integTestAction);
+      integTestAction.onStateChange(
+        "IntegrationTestFailed",
+        new SnsTopic(this.pipelineNotificationsTopic, {
+          message: RuleTargetInput.fromText(
+            `Integration Test Failed. See details here: ${EventField.fromPath(
+              "$.detail.execution-result.external-execution-url"
+            )}`
+          ),
+        }),
+        {
+          ruleName: "IntegrationTestFailed",
+          eventPattern: {
+            detail: {
+              state: ["FAILED"],
+            },
+          },
+          description: "Integration test has failed",
+        }
+      );
+    }
+  
+    
+    
+
+  
+  }
 
 
 
     
    
-  }
-}
+  
+
+  
+
