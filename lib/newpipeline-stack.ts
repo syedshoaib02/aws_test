45c76fe90480f43c3d0c3d5b4ec8d2ef964e2a85
Syed Shoaib <syed.shoaib@trangile.com>
2023-02-09 00:41:13 +0530

    added2

--- lib/newpipeline-stack.ts
+++ lib/newpipeline-stack.ts
@@ -132,24 +132,26 @@ export class NewpipelineStack extends cdk.Stack {
     //     description: "Integration test has failed by syed",
     //   }
     // );
-    const DATE = new Date().toLocaleDateString("en-US", {
-      day: "2-digit",
-      month: "2-digit",
-      year: "numeric",
-    });
-    const TIME = new Date().toLocaleTimeString("en-US", {
-      hour: "2-digit",
-      minute: "2-digit",
-    });
+    // const DATE = new Date().toLocaleDateString("en-US", {
+    //   day: "2-digit",
+    //   month: "2-digit",
+    //   year: "numeric",
+    // });
+    // const TIME = new Date().toLocaleTimeString("en-US", {
+    //   hour: "2-digit",
+    //   minute: "2-digit",
+    // });
 
-    const COMMIT_ID=process.env.COMMIT_ID
-    const REPORT_NAME = `ppl-Report-${DATE}-${COMMIT_ID}html`;
-    const ENCODED_REPORT_NAME = encodeURIComponent(REPORT_NAME);
+    // const COMMIT_ID=process.env.COMMIT_ID
+    // const REPORT_NAME = `ppl-Report-${DATE}-${COMMIT_ID}html`;
+    // const ENCODED_REPORT_NAME = encodeURIComponent(REPORT_NAME);
     
     
     const bucketName = 'newpipelinestack-pipelineartifactsbucket22248f97-dttshkqq1xz2';
 const reportKey = 'newpipelinestack-pipelineartifactsbucket22248f97-dttshkqq1xz2/reports';
 const htmlReportKey = `newpipelinestack-pipelineartifactsbucket22248f97-dttshkqq1xz2.s3.ap-south-1.amazonaws.com/reports/PPL_Report.html`;
+
+
 
     
 
