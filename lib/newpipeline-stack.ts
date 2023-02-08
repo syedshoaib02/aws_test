9d170a4ea6a17d24e85266837b355c0c64bb45e3
Syed Shoaib <syed.shoaib@trangile.com>
2023-02-09 00:09:03 +0530

    added2

--- lib/newpipeline-stack.ts
+++ lib/newpipeline-stack.ts
@@ -141,10 +141,7 @@ export class NewpipelineStack extends cdk.Stack {
       hour: "2-digit",
       minute: "2-digit",
     });
-    // const execSync = require("child_process").execSync;
-    // const COMMIT_ID = execSync("git rev-parse --short HEAD").toString().trim();
-    // console.log("Git commit ID:", COMMIT_ID);
-   
+
     const COMMIT_ID=process.env.COMMIT_ID
     const REPORT_NAME = `ppl-Report-${DATE}-${COMMIT_ID}html`;
     const ENCODED_REPORT_NAME = encodeURIComponent(REPORT_NAME);
