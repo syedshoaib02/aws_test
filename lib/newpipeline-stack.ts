0f902087210f4c286f2ac94d444513ef146a5060
Syed Shoaib <syed.shoaib@trangile.com>
2023-02-09 00:20:17 +0530

    added2

--- lib/newpipeline-stack.ts
+++ lib/newpipeline-stack.ts
@@ -149,7 +149,7 @@ export class NewpipelineStack extends cdk.Stack {
     
     const bucketName = 'newpipelinestack-pipelineartifactsbucket22248f97-dttshkqq1xz2';
 const reportKey = 'newpipelinestack-pipelineartifactsbucket22248f97-dttshkqq1xz2/reports';
-const htmlReportKey = `newpipelinestack-pipelineartifactsbucket22248f97-dttshkqq1xz2.s3.ap-south-1.amazonaws.com/reports/${ENCODED_REPORT_NAME}`;
+const htmlReportKey = `newpipelinestack-pipelineartifactsbucket22248f97-dttshkqq1xz2.s3.ap-south-1.amazonaws.com/reports/PPL_Report.html`;
 
     
 
